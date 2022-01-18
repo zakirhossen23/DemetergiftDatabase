65e12f9b4c56e6be0824a4f5354e31e86079cf1a
Zakir Hossen <73053667+zakirhossen23@users.noreply.github.com>
2022-01-18 18:39:18 +0600

    Initial commit

---
+++ pages/api/create.js
@@ -0,0 +1,22 @@
+import prisma from "../../lib/prisma";
+
+export default async function handle(req, res) {
+  const { title, description, endDate, Goal, logolink } = req.body;
+  if (title) {
+    const result = await prisma.event.create({
+      data: {
+        title: title,
+        description: description,
+        endDate: endDate,
+        Goal: Goal,
+        logolink: logolink
+      },
+    });
+    res.json(result);
+  }
+  return (
+    <>
+      <h1>Please use post method</h1>
+    </>
+  )
+}
