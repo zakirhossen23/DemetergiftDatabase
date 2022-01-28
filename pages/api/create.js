606a954f1476615895dabba408ad9b522eab3475
unknown <zakirhoss12345@gmail.com>
2022-01-29 02:31:18 +0600

    added wallettype

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,7 +1,7 @@
 import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
-  const { title, description, endDate, Goal, logolink, wallet } = req.body;
+  const { title, description, endDate, Goal, logolink, wallet,wallettype } = req.body;
   if (title) {
     const result = await prisma.event.create({
       data: {
@@ -11,6 +11,7 @@ export default async function handle(req, res) {
         Goal: Goal,
         logolink: logolink,
         wallet: wallet,
+        wallettype: wallettype,
       },
     });
     res.json(result);
