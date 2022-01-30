9573a054f8c220cfb9df160e09676f03fff25d8a
unknown <zakirhoss12345@gmail.com>
2022-01-31 03:49:28 +0600

    eventcategories

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,7 +1,7 @@
 import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
-  const { title, description, endDate, Goal, logolink, wallet,wallettype } = req.body;
+  const { title, description, endDate, Goal, logolink, wallet,wallettype ,eventcategories} = req.body;
   if (title) {
     const result = await prisma.event.create({
       data: {
@@ -12,6 +12,7 @@ export default async function handle(req, res) {
         logolink: logolink,
         wallet: wallet,
         wallettype: wallettype,
+        eventcategories:eventcategories
       },
     });
     res.json(result);
